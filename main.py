1dfcb261d4ed55cf04d15a66bb74471e982a1c22
Omar Terrazas <terrazas.omar@gmail.com>
2022-02-07 22:17:58 -0600

    Validando con Enum

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Python
 from typing import Optional
-from unittest import result
+from enum import Enum
 
 # Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # FastAPI
 from fastapi import FastAPI, Body, Path, Query
@@ -12,12 +12,19 @@ app = FastAPI()
 
 # Models
 
+class HairColor(Enum):
+    white = 'white'
+    brown = 'brown'
+    black = 'black'
+    blonde = 'blonde'
+    red = 'red'
+
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(..., min_length=1, max_length=50)
+    last_name: str = Field(..., min_length=1, max_length=50)
+    age: int = Field(..., gt=0, le=115)
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 class Location(BaseModel):
     city: str
@@ -58,3 +65,4 @@ def update_person(
     results = person.dict()
     results.update(location.dict())
     return results
+
